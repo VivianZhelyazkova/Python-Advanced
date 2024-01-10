048723e9de2824d81e8477cff9500ca40f247f43
Viviancat666 <vivian9069@gmail.com>
2024-01-11 01:41:10 +0200

    Update balanced_parentheses.py

--- lists_as_stacks_and_queues_exercise/balanced_parentheses.py
+++ lists_as_stacks_and_queues_exercise/balanced_parentheses.py
@@ -1,8 +1,17 @@
 sequence = input()
 parentheses = []
 symbols = {")": "(", "}": "{", "]": "["}
+is_balanced = True
 for char in sequence:
-    if char == "(":
+    if char not in symbols:
         parentheses.append(char)
-    elif char == ")":
-        parentheses.pop()
+    else:
+        popped = parentheses.pop() if parentheses else ""
+        if popped != symbols[char]:
+            is_balanced = False
+
+if is_balanced and not parentheses:
+    print("YES")
+else:
+    print("NO")
+

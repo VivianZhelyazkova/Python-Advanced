37120e8187168aa81d1e1e54182bcd19245970ed
Viviancat666 <vivian9069@gmail.com>
2024-01-12 00:41:33 +0200

    Create key_revolver.py

---
+++ lists_as_stacks_and_queues_exercise/key_revolver.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+bullet_price = int(input())
+barrel_size = int(input())
+
+bullets = [int(x) for x in input().split()]
+locks = deque([int(x) for x in input().split()])
+
+value = int(input())
+

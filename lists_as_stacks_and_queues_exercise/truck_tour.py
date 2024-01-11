c242a5c7382a46a7bfec7c12bf3621e86fc9d1ad
Viviancat666 <vivian9069@gmail.com>
2024-01-11 02:13:23 +0200

    Update truck_tour.py

--- lists_as_stacks_and_queues_exercise/truck_tour.py
+++ lists_as_stacks_and_queues_exercise/truck_tour.py
@@ -2,11 +2,23 @@ from collections import deque
 
 number_of_pumps = int(input())
 pumps = deque()
+current_liters = 0
 
-for _ in range(number_of_pumps):
-    pumps.append(input().split())
+for index in range(number_of_pumps):
+    info = [int(x) for x in input().split()]
+    info.append(index)
 
-for pump in pumps:
-    liters, kilometers = pump
-    if liters >= kilometers:
-        start = 
+counter = 0
+
+while counter < len(pumps):
+
+    liters, kilometers, index = pumps[counter]
+    current_liters += liters
+    if kilometers > current_liters:
+        pumps.rotate(-1)
+        current_liters = 0
+        counter = 0
+        continue
+    else:
+        current_liters -= kilometers
+    counter += 1

31543499916afcafc3b2a0c83923bc2dc012bba8
Viviancat666 <vivian9069@gmail.com>
2024-01-12 16:35:09 +0200

    Update robotics.py

--- lists_as_stacks_and_queues_exercise/robotics.py
+++ lists_as_stacks_and_queues_exercise/robotics.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 robots = {}
 
 for r in input().split(';'):
-    name,time = r.split("-")
+    name, time = r.split("-")
     robots[name] = [int(time), 0]
 
 factory_time = datetime.strptime(input(), "%H:%M:%S")
@@ -16,3 +16,16 @@ while product != "End":
     products.append(product)
     product = input()
 
+while products:
+
+    factory_time += timedelta(seconds=1)
+    product = products.popleft()
+
+    free_robots = []
+
+    for name, info in robots.items():
+        if info[1] != 0:
+            robots[name][1] -= 1
+
+        if info[1] == 0:
+            free_robots.append([name, info])

3516bd5040b5662b7f09c72208013327f35bb309
Viviancat666 <vivian9069@gmail.com>
2024-01-16 01:14:21 +0200

    Create numbers.py

---
+++ stacks_queues_tuples_sets_exercise/numbers.py
@@ -0,0 +1,10 @@
+set_1 = set(int(x) for x in input().split())
+set_2 = set(int(x) for x in input().split())
+
+number = int(input())
+
+
+for _ in range(number):
+    command = input()
+    cmd1, cmd2, numbers = command.split()
+    numbers_list

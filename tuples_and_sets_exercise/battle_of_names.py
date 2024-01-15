24b8a7270ed0c653cc25a85baaabd9600eeaaa9c
Viviancat666 <vivian9069@gmail.com>
2024-01-15 19:51:43 +0200

    Update battle_of_names.py

--- tuples_and_sets_exercise/battle_of_names.py
+++ tuples_and_sets_exercise/battle_of_names.py
@@ -2,9 +2,19 @@ number = int(input())
 set_odd = set()
 set_even = set()
 
-for _ in range(number):
+for num in range(number):
     name = input()
+    row = num + 1
+    name_sum = int(sum(map(ord, name)) / row)
+    if name_sum % 2 == 0:
+        set_even.add(name_sum)
+    else:
+        set_odd.add(name_sum)
 
-
-    for char in name:
+if sum(set_even) == sum(set_odd):
+    print(*set_even.union(set_odd), sep=", ")
+elif sum(set_even) < sum(set_odd):
+    print(*set_odd.difference(set_even), sep=", ")
+else:
+    print(*set_odd.symmetric_difference(set_even),sep=", ")
 

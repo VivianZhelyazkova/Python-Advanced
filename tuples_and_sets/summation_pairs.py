b83d2c2cc41c185e311a66cfe49985c91fc04530
Viviancat666 <vivian9069@gmail.com>
2024-01-15 04:19:40 +0200

    Update summation_pairs.py

--- tuples_and_sets/summation_pairs.py
+++ tuples_and_sets/summation_pairs.py
@@ -1,9 +1,13 @@
 numbers = [int(x) for x in input().split()]
 target = int(input())
 
-for index, number in enumerate(numbers):
-    for i in range(index + 1, len(numbers)):
-        if number + numbers[i] == target:
-            print(f"{number} + {numbers[i]} = {target}")
+# for index, number in enumerate(numbers):
+#     for i in range(index + 1, len(numbers)):
+#         if number + numbers[i] == target:
+#             print(f"{number} + {numbers[i]} = {target}")
 
+for number in numbers:
 
+    if target - number in numbers:
+        print(f"{number} + {target - number} = {target}")
+        numbers.remove(number)

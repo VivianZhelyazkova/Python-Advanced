aaf694ec33370e2a5abc173abc52f839a31c98ab
Viviancat666 <vivian9069@gmail.com>
2024-01-16 01:22:10 +0200

    Update numbers.py

--- stacks_queues_tuples_sets_exercise/numbers.py
+++ stacks_queues_tuples_sets_exercise/numbers.py
@@ -6,5 +6,22 @@ number = int(input())
 
 for _ in range(number):
     command = input()
+    if "Check" in command:
+
+
     cmd1, cmd2, numbers = command.split()
-    numbers_list
+    numbers_list = (int(x) for x in numbers.split())
+
+    if cmd1 == "Add":
+
+        if cmd2 == "First":
+            set_1.union(numbers_list)
+        elif cmd2 == "Second":
+            set_2.union(numbers_list)
+
+    elif cmd1 == "Remove":
+
+        if cmd2 == "First":
+            set_1.difference_update(numbers_list)
+        elif cmd2 == "Second":
+            set_2 - set(numbers_list)

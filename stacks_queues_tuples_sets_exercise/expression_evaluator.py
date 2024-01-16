debfabd804ae42cd198e7635f0efbaa36686d225
Viviancat666 <vivian9069@gmail.com>
2024-01-16 19:24:41 +0200

    Update expression_evaluator.py

--- stacks_queues_tuples_sets_exercise/expression_evaluator.py
+++ stacks_queues_tuples_sets_exercise/expression_evaluator.py
@@ -5,7 +5,7 @@ expression = input().split()
 expression_copy = expression.copy()
 last_operator_index = 0
 
-current_sum = ""
+current_sum = None
 
 for index, char in enumerate(expression):
 
@@ -19,14 +19,22 @@ for index, char in enumerate(expression):
         numbers_as_str = expression[start:end]
         numbers = list(int(x) for x in numbers_as_str)
 
-        if current_sum:
-            numbers.insert(0,current_sum)
+        if current_sum is not None:
+            numbers.insert(0, current_sum)
 
         if char == "*":
             current_sum = math.prod(numbers)
         elif char == "+":
             current_sum = sum(numbers)
         elif char == "-":
-            total = 0
-            for num in numbers:
-                total += num
+            total = numbers[0]
+            for i in range(1, len(numbers)):
+                total -= numbers[i]
+            current_sum = total
+        elif char == "/":
+            total = numbers[0]
+            for i in range(1, len(numbers)):
+                total /= numbers[i]
+            current_sum = int(total)
+
+print(current_sum)

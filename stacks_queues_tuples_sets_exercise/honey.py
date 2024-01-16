beb01f5744f1415e357f827fc127ee4bfbf5d3db
Viviancat666 <vivian9069@gmail.com>
2024-01-16 22:05:13 +0200

    Create honey.py

---
+++ stacks_queues_tuples_sets_exercise/honey.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+bees = deque(int(x) for x in input().split())
+nectar = deque(int(x) for x in input().split())
+symbols = deque(input().split())
+
+def operations(bee,nect,symb)
+    if 
+
+total_honey = 0
+
+while bees and nectar:
+
+    current_bee = bees[0]
+    current_nectar = nectar[-1]
+
+    if current_nectar >= current_bee:

608e16f3e2309319d253808efef7aa2f82413fc1
Viviancat666 <vivian9069@gmail.com>
2024-01-16 22:20:10 +0200

    Update honey.py

--- stacks_queues_tuples_sets_exercise/honey.py
+++ stacks_queues_tuples_sets_exercise/honey.py
@@ -4,8 +4,19 @@ bees = deque(int(x) for x in input().split())
 nectar = deque(int(x) for x in input().split())
 symbols = deque(input().split())
 
-def operations(bee,nect,symb)
-    if 
+
+def making_honey(bee, nect, symb):
+    result = 0
+    if symb == '+':
+        result = bee + nect
+    elif symb == '-':
+        result = bee - nect
+    elif symb == '*':
+        result = bee * nect
+    elif symb == '/':
+        result = bee / nect
+    return abs(result)
+
 
 total_honey = 0
 
@@ -15,3 +26,8 @@ while bees and nectar:
     current_nectar = nectar[-1]
 
     if current_nectar >= current_bee:
+        symbol = symbols.popleft()
+        if current_nectar > 0:
+            honey = making_honey(current_bee, current_nectar, symbol)
+        bees.popleft()
+        nectar.pop()

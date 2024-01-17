16634e99a930c742fee6bfd5d720e0e95a8056a9
Viviancat666 <vivian9069@gmail.com>
2024-01-17 03:15:18 +0200

    Create paint_colors.py

---
+++ stacks_queues_tuples_sets_exercise/paint_colors.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+string = deque(input().split())
+
+main_colors = ["red", "yellow", "blue"]
+secondary_colors = ["orange", "purple", "green"]
+
+main_colors_made = []
+secondary_colors_made = []

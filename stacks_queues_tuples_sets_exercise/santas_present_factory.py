2da1936a9259972bcd9a6cf62fccf58f116c2e40
Viviancat666 <vivian9069@gmail.com>
2024-01-17 02:35:13 +0200

    Update santas_present_factory.py

--- stacks_queues_tuples_sets_exercise/santas_present_factory.py
+++ stacks_queues_tuples_sets_exercise/santas_present_factory.py
@@ -5,39 +5,25 @@ magic = deque(int(x) for x in input().split())
 
 presents = {150: "Doll", 250: "Wooden train", 300: "Teddy bear", 400: "Bicycle"}
 crafted_presents = {"Bicycle": 0, "Doll": 0, "Teddy bear": 0, "Wooden train": 0}
-is_empty = False
+
 
 while materials and magic:
 
-    current_box = materials[-1]
-    current_magic = magic[0]
-
-    if current_box == 0:
-        materials.pop()
-
-    if current_magic == 0:
-        magic.popleft()
-
-    if is_empty:
-        break
+    current_box = materials.pop()
+    current_magic = magic.popleft()
 
     magic_level = current_box * current_magic
 
     if magic_level in presents:
-        materials.pop()
-        magic.popleft()
         present = presents[magic_level]
         crafted_presents[present] += 1
 
     elif magic_level < 0:
-        materials.pop()
-        magic.popleft()
         new_value = current_box + current_magic
         materials.append(new_value)
 
     elif magic_level not in presents and magic_level > 0:
-        magic.popleft()
-        materials[-1] += 15
+        materials.append(current_box + 15)
 
 pair_made = False
 

987c201b8a89d3e561b3296cafbec611ff1e76f3
Viviancat666 <vivian9069@gmail.com>
2024-01-18 21:12:42 +0200

    Update symbol_in_matrix.py

--- multidimensional_lists/symbol_in_matrix.py
+++ multidimensional_lists/symbol_in_matrix.py
@@ -5,17 +5,18 @@ for row in range(rows):
     matrix.append(list(input()))
 
 symbol = input()
-
-is_found = False
+#
+# is_found = False
 
 for y in range(rows):
     for x in range(rows):
         if matrix[y][x] == symbol:
             print(f"({y}, {x})")
-            is_found = True
-            break
-    if is_found:
-        break
+            # is_found = True
+            # break
+            exit()
+    # if is_found:
+    #     break
 
-if not is_found:
+else:
     print(f"{symbol} does not occur in the matrix")

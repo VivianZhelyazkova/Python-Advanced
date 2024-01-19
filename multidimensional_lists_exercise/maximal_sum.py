4f50b27b1e8c4e31ddc7f20d0be20e5f49f8a542
Viviancat666 <vivian9069@gmail.com>
2024-01-19 03:59:18 +0200

    Update maximal_sum.py

--- multidimensional_lists_exercise/maximal_sum.py
+++ multidimensional_lists_exercise/maximal_sum.py
@@ -1,21 +1,39 @@
+import sys
+
 rows, cols = [int(x) for x in input().split()]
 matrix = []
 
 for row in range(rows):
-    matrix.append(input().split())
+    matrix.append([int(x) for x in input().split()])
 
-result = float("-inf")
-values = []
+result = {}
+max_value = -sys.maxsize
+
+
+def making_submatrix(x, y, matrix):
+    length = 3
+    submatrix = []
+    current_sum = 0
+    for i in range(y, y + length):
+        current_row = []
+        for j in range(x, x + length):
+            current_row.append(matrix[i][j])
+            current_sum += matrix[i][j]
+        submatrix.append(current_row)
+    current_result = {current_sum: submatrix}
+    return current_result
+
 
 for y in range(rows - 2):
     for x in range(cols - 2):
-        top_one = matrix[y][x]
-        top_two = matrix[y][x + 1]
-        top_three = matrix[y][x + 2]
-        bottom_one = matrix[y + 1][x]
-        bottom_two = matrix[y + 1][x + 1]
-        bottom_three = matrix[y + 1][x + 2]
-        current_result = top_one + top_
-        if current_result > result:
+        current_result = making_submatrix(x, y, matrix)
+        current_max = int(list(current_result.keys())[0])
+        if current_max > max_value:
+            max_value = current_max
             result = current_result
-            values = [[top_left, top_right], [bottom_left, bottom_right]]
+
+print(f"Sum = {max_value}")
+
+for sum, matrix in current_result.items():
+    for row in matrix:
+        print(*row, sep=" ")

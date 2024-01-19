449e1edaa19b7e04adeb5ca24e3258099d410c83
Viviancat666 <vivian9069@gmail.com>
2024-01-19 19:14:24 +0200

    Update diagonals.py

--- multidimensional_lists_exercise/diagonals.py
+++ multidimensional_lists_exercise/diagonals.py
@@ -1,13 +1,12 @@
 rows = int(input())
 
-matrix = [[int() for x in input().split(", ")] for row in range(rows)]
+matrix = [[int(x) for x in input().split(", ")] for row in range(rows)]
 
 primary_diagonal = [matrix[x][x] for x in range(rows)]
-secondary_diagonal = []
+secondary_diagonal = [matrix[x][rows-1-x] for x in range(rows)]
 
-
-for x in range(rows):
-    secondary_diagonal.append(matrix[x][rows-1-x])
+# for x in range(rows):
+#     secondary_diagonal.append(matrix[x][rows-1-x])
 
 print(f"Primary diagonal: {', '.join(str(x) for x in primary_diagonal)}. Sum: {sum(primary_diagonal)}")
 print(f"Secondary diagonal: {', '.join(str(x) for x in secondary_diagonal)}. Sum: {sum(secondary_diagonal)}")

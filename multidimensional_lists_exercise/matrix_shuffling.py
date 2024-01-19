8947009839973cbcd4872bc1613f8ca8b8ad1f24
Viviancat666 <vivian9069@gmail.com>
2024-01-19 21:20:36 +0200

    Update matrix_shuffling.py

--- multidimensional_lists_exercise/matrix_shuffling.py
+++ multidimensional_lists_exercise/matrix_shuffling.py
@@ -11,11 +11,9 @@ regex = re.compile(regex)
 while command != "END":
     match = regex.match(command)
     if match:
-        cmd, row_1, col_1, row_2, col_2 = command.split()
-        row_1 = int(row_1)
-        col_1 = int(col_1)
-        row_2 = int(row_2)
-        col_2 = int(col_2)
+        cmd, *numbers = command.split()
+        numbers = [int(x) for x in numbers]
+        row_1, col_1, row_2, col_2 = numbers
         if row_1 < rows and row_2 < rows and col_1 < cols and col_2 < cols:
             matrix[row_1][col_1],matrix[row_2][col_2] = matrix[row_2][col_2],matrix[row_1][col_1]
             [print(*row) for row in matrix]

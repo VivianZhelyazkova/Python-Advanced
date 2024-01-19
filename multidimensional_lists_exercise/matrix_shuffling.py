ef9ccde5f44b12bf783357bbaefcad4b49cde559
Viviancat666 <vivian9069@gmail.com>
2024-01-19 21:15:33 +0200

    Update matrix_shuffling.py

--- multidimensional_lists_exercise/matrix_shuffling.py
+++ multidimensional_lists_exercise/matrix_shuffling.py
@@ -2,16 +2,25 @@ import re
 
 rows, cols = [int(x) for x in input().split()]
 
-matrix = [int(x) for x in input().split() for row in range(rows)]
+matrix = [input().split() for row in range(rows)]
 
 command = input()
-regex = r"/^swap( \d+){4}/gm"
+regex = r"^swap( \d+){4}$"
+regex = re.compile(regex)
 
 while command != "END":
-    match = re.search(command, regex)
-    print(match)
+    match = regex.match(command)
     if match:
-        print(command)
+        cmd, row_1, col_1, row_2, col_2 = command.split()
+        row_1 = int(row_1)
+        col_1 = int(col_1)
+        row_2 = int(row_2)
+        col_2 = int(col_2)
+        if row_1 < rows and row_2 < rows and col_1 < cols and col_2 < cols:
+            matrix[row_1][col_1],matrix[row_2][col_2] = matrix[row_2][col_2],matrix[row_1][col_1]
+            [print(*row) for row in matrix]
+        else:
+            print("Invalid input!")
     else:
         print("Invalid input!")
     command = input()

20f57951154815376bd68eed826f4b65f77f71d8
Viviancat666 <vivian9069@gmail.com>
2024-01-19 02:50:36 +0200

    Update squares_in_matrix.py

--- multidimensional_lists_exercise/squares_in_matrix.py
+++ multidimensional_lists_exercise/squares_in_matrix.py
@@ -4,3 +4,15 @@ matrix = []
 for row in range(rows):
     matrix.append(input().split())
 
+squares = 0
+
+for y in range(rows - 1):
+    for x in range(cols - 1):
+        top_left = matrix[y][x]
+        top_right = matrix[y][x + 1]
+        bottom_left = matrix[y + 1][x]
+        bottom_right = matrix[y + 1][x + 1]
+        if top_left == top_right and bottom_left == bottom_right and top_right == bottom_right:
+            squares += 1
+
+print(squares)

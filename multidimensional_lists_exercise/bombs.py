2f361701547547b12f7d361e0ebf5bf76a1bbb5d
Viviancat666 <vivian9069@gmail.com>
2024-01-20 21:35:34 +0200

    Update bombs.py

--- multidimensional_lists_exercise/bombs.py
+++ multidimensional_lists_exercise/bombs.py
@@ -7,10 +7,10 @@ indices = input().split()
 indices_matrix = []
 
 
-def are_coordinates_valid(x, y, some_matrix):
+def are_coordinates_valid(x, y, lenght):
     if x < 0 or y < 0:
         return False
-    if x > len(some_matrix[0]) or y > len(some_matrix[0]):
+    if x > lenght or y > lenght:
         return False
     return True
 
@@ -21,10 +21,26 @@ def generate_bomb_hit_box(row, col):
     return hit_box
 
 
+def validate_coordinates(some_list):
+    valid_coords = []
+    for coord in some_list:
+        if are_coordinates_valid(coord[0], coord[1], n):
+            valid_coords.append(coord)
+    return valid_coords
+
+
 for el in indices:
     indices_matrix.append([int(n) for n in el.split(",")])
 
 for coordinate in indices_matrix:
     row, col = coordinate
     bomb = matrix[row][col]
+    if bomb <= 0:
+        continue
     bomb_hit_box = generate_bomb_hit_box(row, col)
+    bomb_hit_box = validate_coordinates(bomb_hit_box)
+    for c in bomb_hit_box:
+        x, y = c
+        if matrix[x][y] > 0:
+            matrix[x][y] -= bomb
+        matrix[row][col] = 0

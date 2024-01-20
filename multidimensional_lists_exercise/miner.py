bfa02b12226d21a4c4aca497ee0625112b7b7ee9
Viviancat666 <vivian9069@gmail.com>
2024-01-20 22:13:44 +0200

    Update miner.py

--- multidimensional_lists_exercise/miner.py
+++ multidimensional_lists_exercise/miner.py
@@ -4,24 +4,36 @@ commands = input().split()
 
 matrix = [[x for x in input().split()] for _ in range(n)]
 
-def get_new_position(str,row,col):
+
+def get_new_position(cmd, position):
+    row, col = position
     coms = {
-        "left": [row,col-1],
-        "right": [row,col +1],
-        "up": [row -1,col],
-        "down": [row +1,col]
-        }
+        "left": [row, col - 1],
+        "right": [row, col + 1],
+        "up": [row - 1, col],
+        "down": [row + 1, col]
+    }
+    return coms[cmd] if is_position_valid(coms[cmd]) else position
 
 
+def is_position_valid(position):
+    row, col = position
+    if row < 0 or col < 0:
+        return False
+    if row > n - 1 or col > n - 1:
+        return False
+    return True
 
-miner_col, miner_row = 0, 0
+
+miner_position = []
 
 for index, row in enumerate(matrix):
     if "s" in row:
         miner_col = row.index("s")
         miner_row = index
+        miner_position.append(miner_row)
+        miner_position.append(miner_col)
 
 for command in commands:
-
 
 [print(*row) for row in matrix]

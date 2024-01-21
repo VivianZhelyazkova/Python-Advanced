1b43b590ea609248810c0952c17cb4203f98e8a9
Viviancat666 <vivian9069@gmail.com>
2024-01-22 01:56:01 +0200

    Update radioactive_mutant_vampire_bunnies.py

--- multidimensional_lists_exercise/radioactive_mutant_vampire_bunnies.py
+++ multidimensional_lists_exercise/radioactive_mutant_vampire_bunnies.py
@@ -1,6 +1,6 @@
 row, col = [int(x) for x in input().split()]
-
 matrix = [[x for x in input()] for x in range(row)]
+commands = [x for x in input()]
 
 PLAYER = "P"
 BUNNY = "B"
@@ -11,6 +11,12 @@ RIGHT = "R"
 UP = "U"
 DOWN = "D"
 
+is_alive = True
+has_escaped = False
+
+while commands and is_alive and not has_escaped:
+
+    
 
 
 

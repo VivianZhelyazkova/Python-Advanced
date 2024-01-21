83adc638db6927c1098ca0f41d9ab8e6861b026e
Viviancat666 <vivian9069@gmail.com>
2024-01-22 01:51:39 +0200

    Create radioactive_mutant_vampire_bunnies.py

---
+++ multidimensional_lists_exercise/radioactive_mutant_vampire_bunnies.py
@@ -0,0 +1,17 @@
+row, col = [int(x) for x in input().split()]
+
+matrix = [[x for x in input()] for x in range(row)]
+
+PLAYER = "P"
+BUNNY = "B"
+EMPTY = "."
+
+LEFT = "L"
+RIGHT = "R"
+UP = "U"
+DOWN = "D"
+
+
+
+
+print(matrix)

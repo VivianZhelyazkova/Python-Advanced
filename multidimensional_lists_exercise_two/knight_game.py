6c5e0b55eb08b54d717b2e3daca48555b0aff441
Viviancat666 <vivian9069@gmail.com>
2024-01-22 21:25:40 +0200

    Update knight_game.py

--- multidimensional_lists_exercise_two/knight_game.py
+++ multidimensional_lists_exercise_two/knight_game.py
@@ -1,23 +1,12 @@
 n = int(input())
 
-matrix = [[x for x in input().split()] for _ in range(n)]
+matrix = [[x for x in input()] for _ in range(n)]
 
 KNIGHT = "K"
 EMPTY = "0"
 
-knights_to_be_removed = 0
 
-
-def find_knight_position(some_matrix):
-    knights_positions = []
-    for row_index, row in enumerate(some_matrix):
-        for col_index, char in enumerate(row):
-            if char == KNIGHT:
-                knights_positions.append([row_index, col_index])
-    return knights_positions
-
-
-def find_knight_attacks(position, some_matrix):
+def find_knight_attacks(position):
     knights_attacks = []
     row, col = position
     knights_attacks.append([row - 2, col + 1])
@@ -31,4 +20,30 @@ def find_knight_attacks(position, some_matrix):
     return knights_attacks
 
 
-knights_positions = find_knight_position(matrix)
+knights_removed = 0
+
+while True:
+    max_attacks = 0
+    max_knight_attacks_pos = []
+
+    for row in range(n):
+        for col in range(n):
+            if matrix[row][col] == KNIGHT:
+                attacks = 0
+                for attack in find_knight_attacks([row, col]):
+                    attack_row, attack_col = attack
+                    if attack_row in range(n) and attack_col in range(n):
+                        if matrix[attack_row][attack_col] == KNIGHT:
+                            attacks += 1
+                if attacks > max_attacks:
+                    max_attacks = attacks
+                    max_knight_attacks_pos = [row, col]
+
+    if max_knight_attacks_pos:
+        matrix[max_knight_attacks_pos[0]][max_knight_attacks_pos[1]] = EMPTY
+        knights_removed += 1
+
+    else:
+        break
+
+print(knights_removed)

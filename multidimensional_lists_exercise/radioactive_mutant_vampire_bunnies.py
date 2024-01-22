cf796f4ae91f8accee3e1b7e761bd7552826391f
Viviancat666 <vivian9069@gmail.com>
2024-01-22 02:33:16 +0200

    Update radioactive_mutant_vampire_bunnies.py

--- multidimensional_lists_exercise/radioactive_mutant_vampire_bunnies.py
+++ multidimensional_lists_exercise/radioactive_mutant_vampire_bunnies.py
@@ -36,11 +36,24 @@ def get_new_position(cmd, position):
     return commands_map[cmd]
 
 
-def escaped(position, x, y):
+def is_within_matrix(position, x, y):
     row, col = position
     return row == x or col == y or row < 0 or col < 0
-   
 
+
+def get_bunnies_positions(some_matrix):
+    bunnies_positions = []
+    for row_index, row in enumerate(some_matrix):
+        for col_index, col in enumerate(row):
+            if col == BUNNY:
+                bunnies_positions.append([row_index, col_index])
+    return bunnies_positions
+
+
+def multiply_bunnies(bunny_positions,some_matrix):
+    for coordinates in bunny_positions:
+        row, col = coordinates
+        
 
 
 is_alive = True
@@ -48,5 +61,8 @@ has_escaped = False
 
 while commands and is_alive and not has_escaped:
     command = commands.popleft()
+    player_position = get_player_position(matrix)
+    new_position = get_new_position(command, player_position)
+    has_escaped = is_within_matrix(new_position, row, col)
 
 print(matrix)

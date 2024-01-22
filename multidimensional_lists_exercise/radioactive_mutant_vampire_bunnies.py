748f7ab0568fb4b2c0e4fd022c461ee1d3d9ed16
Viviancat666 <vivian9069@gmail.com>
2024-01-22 02:11:55 +0200

    Update radioactive_mutant_vampire_bunnies.py

--- multidimensional_lists_exercise/radioactive_mutant_vampire_bunnies.py
+++ multidimensional_lists_exercise/radioactive_mutant_vampire_bunnies.py
@@ -1,6 +1,8 @@
+from collections import deque
+
 row, col = [int(x) for x in input().split()]
 matrix = [[x for x in input()] for x in range(row)]
-commands = [x for x in input()]
+commands = deque(input())
 
 PLAYER = "P"
 BUNNY = "B"
@@ -11,13 +13,35 @@ RIGHT = "R"
 UP = "U"
 DOWN = "D"
 
+
+def get_player_position(some_matrix):
+    player_position = []
+    for row_index, row in enumerate(some_matrix):
+        if PLAYER in row:
+            player_row = row_index
+            player_col = row.index(PLAYER)
+            player_position.append(player_row)
+            player_position.append(player_col)
+    return player_position
+
+
+def get_new_position(cmd, position):
+    row, col = position
+    commands_map = {
+        LEFT: [row, col - 1],
+        RIGHT: [row, col + 1],
+        UP: [row - 1, col],
+        DOWN: [row + 1, col]
+    }
+    return commands_map[cmd]
+
+
+
+
 is_alive = True
 has_escaped = False
 
 while commands and is_alive and not has_escaped:
-
-    
-
-
+    command = commands.popleft()
 
 print(matrix)

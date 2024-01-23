4bf680d5b1034c52075a048e19b2161316b7f7c0
Viviancat666 <vivian9069@gmail.com>
2024-01-23 16:58:54 +0200

    Create range_day.py

---
+++ multidimensional_lists_exercise_two/range_day.py
@@ -0,0 +1,13 @@
+n = 5
+matrix = [[x for x in input().split()] for _ in range(n)]
+number_of_commands = int(input())
+
+SHOOTER = "A"
+TARGET = "x"
+EMPTY = "."
+
+for _ in range(number_of_commands):
+
+    command = input()
+    
+

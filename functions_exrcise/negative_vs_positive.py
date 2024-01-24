600c9cd8e76825b8d89bba3f8a6c30e520516167
Viviancat666 <vivian9069@gmail.com>
2024-01-24 21:18:00 +0200

    Create negative_vs_positive.py

---
+++ functions_exrcise/negative_vs_positive.py
@@ -0,0 +1,23 @@
+nums_input = input().split()
+
+
+def positive_or_negative(numbers):
+    positive = sum([int(x) for x in numbers if int(x) > 0])
+    negative = sum([int(x) for x in numbers if int(x) < 0])
+    return positive, negative
+
+
+def print_sums(neg, pos):
+    print(f"{neg}\n{pos}")
+
+
+def find_strongest(pos, neg):
+    if abs(neg) > abs(pos):
+        print("The negatives are stronger than the positives")
+    else:
+        print("The positives are stronger than the negatives")
+
+
+positives, negatives = positive_or_negative(nums_input)
+print_sums(negatives, positives)
+find_strongest(positives, negatives)

1d54d9fb1b45ad56da1ec8ec946c7f9a72ad7631
Viviancat666 <vivian9069@gmail.com>
2024-01-25 19:31:00 +0200

    Update numbers_filter.py

--- functions_exrcise/numbers_filter.py
+++ functions_exrcise/numbers_filter.py
@@ -1,8 +1,9 @@
 def even_odd_filter(**kwargs):
-    for cmd, nums in kwargs.items():
-        if cmd == 'odd':
-            kwargs['odd'] = [x for x in nums if x % 2 != 0]
-        elif cmd == 'even':
-            kwargs['even'] = [x for x in nums if x % 2 == 0]
+
+    if 'odd' in kwargs:
+        kwargs['odd'] = [x for x in kwargs.values() if x % 2 != 0]
+    if 'even' in kwargs:
+        kwargs['even'] = [x for x in kwargs.values() if x % 2 == 0]
+        
     return dict(sorted(kwargs.items(), key=lambda x: len(x[1]), reverse=True))
 

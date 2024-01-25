9c27902880011493f47270099df4d3b328db361b
Viviancat666 <vivian9069@gmail.com>
2024-01-25 02:41:10 +0200

    Update math_operations.py

--- functions_exrcise/math_operations.py
+++ functions_exrcise/math_operations.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 def operations(operator, number, some_dict):
     if operator == "a":
         some_dict[operator] += number
@@ -19,18 +16,12 @@ def math_operations(*args, **kwargs):
         if index == len(kwargs):
             index = 0
         key = list(kwargs.keys())[index]
-        if key == "a":
-            kwargs[key] += num
-        elif key == "s":
-            kwargs[key] -= num
-        elif key == "d":
-            if num != 0:
-                kwargs[key] /= num
-        elif key == "m":
-            kwargs[key] *= num
+        operations(key, num, kwargs)
         index += 1
     sorted_dict = dict(sorted(kwargs.items(), key=lambda x: (-x[1], x[0])))
     result = ""
     for key, val in sorted_dict.items():
         result += f"{key}: {val:.1f}\n"
     return result
+
+

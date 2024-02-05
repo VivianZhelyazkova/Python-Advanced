c3db7442213fddde4f879ff721c5dbbaafb0a6cd
Viviancat666 <vivian9069@gmail.com>
2024-02-06 01:54:45 +0200

    Update line_numbers.py

--- file_handling_exercise/line_numbers.py
+++ file_handling_exercise/line_numbers.py
@@ -3,3 +3,15 @@ import os
 text_path = os.path.join("resources", "text.txt")
 output_path = os.path.join("resources", "output.txt")
 
+try:
+    with open(text_path) as f:
+        lines = f.readlines()
+
+except FileNotFoundError:
+    print(f"Fine {text_path} not found!")
+
+for index, line in enumerate(lines):
+    letters = 0
+    punct_marks = 0
+
+    new_line = f"Line {index + 1}: {line}

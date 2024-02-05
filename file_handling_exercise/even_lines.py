a7f16378cd56e5f2f14b009b19cceef2f5838409
Viviancat666 <vivian9069@gmail.com>
2024-02-06 01:44:12 +0200

    Update even_lines.py

--- file_handling_exercise/even_lines.py
+++ file_handling_exercise/even_lines.py
@@ -5,9 +5,17 @@ path = os.path.join("resources", "text.txt")
 symbols = {"-", ",", ".", "!", "?"}
 REPLACE = "@"
 
-with open(path) as f:
-    content = [line for index, line in enumerate(f.readlines()) if index % 2 == 0]
+try:
+    with open(path) as f:
+        content = [line for index, line in enumerate(f.readlines()) if index % 2 == 0]
+except FileNotFoundError:
+    print(f"File '{path}' not found.")
 
-for line in content:
+for index, line in enumerate(content):
+    current_line = line
     for symbol in symbols:
-        
+        current_line = current_line.replace(symbol, REPLACE)
+    current_line = " ".join(reversed(current_line.split()))
+    content[index] = current_line
+
+print(*content, sep="\n")

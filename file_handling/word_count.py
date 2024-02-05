cf067b520cb8712c09daa168046b84c7987b941a
Viviancat666 <vivian9069@gmail.com>
2024-02-05 22:12:31 +0200

    Update word_count.py

--- file_handling/word_count.py
+++ file_handling/word_count.py
@@ -11,5 +11,18 @@ with open(input_path, "w") as file:
                "-Is this some kind of joke?! Is it?\n"
                "-Quick, hide here…It is safer.")
 
-
 words = open(words_path)
+words_as_list = words.read().lower().split()
+words_count = {}
+inpt = open(input_path)
+inpt_as_str = inpt.read().lower().split()
+
+for word in words_as_list:
+    words_count[word] = inpt_as_str.count(word)
+
+words.close()
+inpt.close()
+
+sorted_words = sorted(words_count.items(), key=lambda x: -x[1])
+
+print(sorted_words)

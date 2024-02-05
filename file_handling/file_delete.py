d38414f1a25612c85fc4bb45cb33b026efbdfd09
Viviancat666 <vivian9069@gmail.com>
2024-02-05 21:54:59 +0200

    Create file_delete.py

---
+++ file_handling/file_delete.py
@@ -0,0 +1,14 @@
+import os
+
+path = os.path.join("my_first_file.txt")
+
+try:
+    os.remove(path)
+
+except FileNotFoundError:
+    print('File already deleted!')
+
+# if os.path.exists(path):
+#     os.remove(path)
+# else:
+#     print('File already deleted!')

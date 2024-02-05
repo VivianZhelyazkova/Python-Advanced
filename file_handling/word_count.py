7beb866ecf07f3c7721067372dcebd5843e8d5ea
Viviancat666 <vivian9069@gmail.com>
2024-02-05 22:40:33 +0200

    Update word_count.py

--- file_handling/word_count.py
+++ file_handling/word_count.py
@@ -1,7 +1,9 @@
 import os
+import re
 
 words_path = os.path.join("files", "words.txt")
 input_path = os.path.join("files", "input.txt")
+output_path = os.path.join("files", "output.txt")
 
 with open(words_path, "w") as file:
     file.write("quick is fault")
@@ -11,18 +13,25 @@ with open(input_path, "w") as file:
                "-Is this some kind of joke?! Is it?\n"
                "-Quick, hide here…It is safer.")
 
+words_count = {}
+
 words = open(words_path)
 words_as_list = words.read().lower().split()
-words_count = {}
+
 inpt = open(input_path)
-inpt_as_str = inpt.read().lower().split()
+inpt_as_str = inpt.read().lower()
 
 for word in words_as_list:
-    words_count[word] = inpt_as_str.count(word)
+    regex = re.compile(rf"\b{word}\b")
+    matches = regex.findall(inpt_as_str)
+    words_count[word] = len(matches)
 
 words.close()
 inpt.close()
 
-sorted_words = sorted(words_count.items(), key=lambda x: -x[1])
+sorted_words = dict(sorted(words_count.items(), key=lambda x: -x[1]))
 
-print(sorted_words)
+with open(output_path, "w") as file:
+    for word, count in sorted_words.items():
+        file.write(f"{word} - {count}\n")
+

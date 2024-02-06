7fc497cbff8ecfe8c5ab221d2d99c9463f510394
Viviancat666 <vivian9069@gmail.com>
2024-02-06 02:16:22 +0200

    Update line_numbers.py

--- file_handling_exercise/line_numbers.py
+++ file_handling_exercise/line_numbers.py
@@ -10,8 +10,15 @@ try:
 except FileNotFoundError:
     print(f"Fine {text_path} not found!")
 
-for index, line in enumerate(lines):
-    letters = 0
-    punct_marks = 0
+with open(output_path,"w") as f:
+    for index, line in enumerate(lines):
+        letters = 0
+        punct_marks = 0
+        for char in line:
+            if char.isalpha():
+                letters += 1
+            elif not char.isalnum() and not char.isspace():
+                punct_marks += 1
+        new_line = f"Line {index + 1}: {line} ({letters})({punct_marks})\n"
+        f.write(new_line)
 
-    new_line = f"Line {index + 1}: {line}

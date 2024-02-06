e15cb3c0c7ef5b708c3dc03eb049a8431baac787
Viviancat666 <vivian9069@gmail.com>
2024-02-06 02:39:51 +0200

    Update file_manipulator.py

--- file_handling_exercise/file_manipulator.py
+++ file_handling_exercise/file_manipulator.py
@@ -13,9 +13,25 @@ while command != "End":
         cmd, file_name, content = command.split("-")
         file_path = os.path.join("resources", file_name)
         with open(file_path, "a") as f:
-            f.write(content)
-            
-    elif "Replace" in command:
+            f.write(content + "\n")
 
+    elif "Replace" in command:
+        cmd, file_name, old_string, new_string = command.split("-")
+        try:
+            file_path = os.path.join("resources", file_name)
+            if os.path.exists(file_path):
+                with open(file_path, "w+") as f:
+                    f.write(f.read().replace(old_string, new_string))
+
+        except FileNotFoundError:
+            print("REPLACEAn error occurred")
+
+    elif "Delete" in command:
+        file_name = command.split("-")[1]
+        file_path = os.path.join("resources", file_name)
+        if os.path.exists(file_path):
+            os.remove(file_name)
+        else:
+            print("DELETEAn error occurred")
 
     command = input()

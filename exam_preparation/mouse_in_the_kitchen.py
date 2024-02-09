1cd774bbf468468d5e5ea653e903e49fd2450ab6
Viviancat666 <vivian9069@gmail.com>
2024-02-09 21:02:24 +0200

    Update mouse_in_the_kitchen.py

--- exam_preparation/mouse_in_the_kitchen.py
+++ exam_preparation/mouse_in_the_kitchen.py
@@ -4,6 +4,14 @@ matrix = []
 for row in range(n):
     matrix.append(list(input()))
 
+
+def move_mouse(old_pos, new_pos):
+    new_row, new_col = new_pos
+    matrix[old_pos[0]][old_pos[1]] = EMPTY
+    matrix[new_row][new_col] = MOUSE
+    return [new_row, new_col]
+
+
 DIRECTIONS = {
     "left": [0, -1],
     "right": [0, 1],
@@ -17,12 +25,16 @@ WALL = "@"
 TRAP = "T"
 
 mouse_position = []
+total_cheese = 0
 for row in range(n):
     for col in range(m):
         if matrix[row][col] == MOUSE:
             mouse_position = [row, col]
+        elif matrix[row][col] == CHEESE:
+            total_cheese += 1
 
 command = input()
+shadow_got_the_mouse = False
 
 while command != "danger":
     next_row = mouse_position[0] + DIRECTIONS[command][0]
@@ -30,13 +42,28 @@ while command != "danger":
 
     if next_row not in range(n) or next_col not in range(m):
         print("No more cheese for tonight!")
+        shadow_got_the_mouse = True
         break
 
     next_position = matrix[next_row][next_col]
 
-    if 
+    if next_position != WALL:
+        mouse_position = move_mouse(mouse_position, [next_row, next_col])
 
+    if next_position == CHEESE:
+        total_cheese -= 1
+        if total_cheese == 0:
+            print("Happy mouse! All the cheese is eaten, good night!")
+            break
 
-
+    elif next_position == TRAP:
+        shadow_got_the_mouse = True
+        print("Mouse is trapped!")
+        break
 
     command = input()
+
+if total_cheese and not shadow_got_the_mouse:
+    print("Mouse will come back later!")
+
+[print(''.join(row)) for row in matrix]

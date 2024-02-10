9a45ad5e87df023efd77bbf3b024614bd952a7b3
Viviancat666 <vivian9069@gmail.com>
2024-02-10 19:20:43 +0200

    Update delivery_boy.py

--- exam_preparation/delivery_boy.py
+++ exam_preparation/delivery_boy.py
@@ -4,14 +4,6 @@ matrix = []
 for row in range(n):
     matrix.append(list(input()))
 
-
-def move_boy(old_pos, new_pos):
-    new_row, new_col = new_pos
-    matrix[old_pos[0]][old_pos[1]] = EMPTY
-    matrix[new_row][new_col] = BOY
-    return [new_row, new_col]
-
-
 DIRECTIONS = {
     "left": [0, -1],
     "right": [0, 1],
@@ -24,6 +16,8 @@ ADDRESS = "A"
 OBSTACLE = "*"
 PIZZA = "P"
 EMPTY = "-"
+READY = "R"
+PATH = "."
 
 boy_position = []
 
@@ -32,9 +26,35 @@ for row in range(n):
         if matrix[row][col] == BOY:
             boy_position = [row, col]
 
+starting_position = boy_position
 
 while True:
     command = input()
-    
+
     next_row = boy_position[0] + DIRECTIONS[command][0]
     next_col = boy_position[1] + DIRECTIONS[command][1]
+
+    if next_row in range(n) and next_col in range(m):
+
+        next_position = matrix[next_row][next_col]
+        if next_position != OBSTACLE:
+            boy_position = [next_row, next_col]
+
+        if next_position == PIZZA:
+            matrix[next_row][next_col] = READY
+            print("Pizza is collected. 10 minutes for delivery.")
+
+        elif next_position == ADDRESS:
+            matrix[next_row][next_col] = PIZZA
+            print("Pizza is delivered on time! Next order...")
+            break
+
+        elif next_position == EMPTY:
+            matrix[next_row][next_col] = PATH
+
+    else:
+        matrix[starting_position[0]][starting_position[1]] = " "
+        print("The delivery is late. Order is canceled.")
+        break
+
+[print("".join(row)) for row in matrix]

deead80d979c6ec285b0d14828da29cf5240b2e8
Viviancat666 <vivian9069@gmail.com>
2024-02-11 03:22:33 +0200

    Create grocery_list.py

---
+++ exam_preparation/grocery_list.py
@@ -0,0 +1,17 @@
+def shop_from_grocery_list(budget, grocery_list, *args):
+
+    for item, price in args:
+        if not budget:
+            break
+        if item in grocery_list:
+            if budget >= price:
+                budget -= price
+                grocery_list.remove(item)
+            else:
+                break
+
+    if not grocery_list:
+        return f"Shopping is successful. Remaining budget: {budget:.2f}."
+
+    return f"You did not buy all the products. Missing products: {', '.join(grocery_list)}."
+

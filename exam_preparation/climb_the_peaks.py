e41db27970a62ada71ebacc7180e00cb2ee87781
Viviancat666 <vivian9069@gmail.com>
2024-02-12 01:17:08 +0200

    Update climb_the_peaks.py

--- exam_preparation/climb_the_peaks.py
+++ exam_preparation/climb_the_peaks.py
@@ -12,21 +12,23 @@ peaks = deque([
 ])
 
 conquered_peaks = []
-
-for index in range(7):
+days = 7
+for index in range(days):
     current_portion = portions.pop()
     current_stamina = stamina.popleft()
     result = current_portion + current_stamina
     if not peaks:
-        print(f"Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK")
         break
     current_peak_level, current_peak_name = peaks[0]
     if result >= current_peak_level:
         conquered_peaks.append(current_peak_name)
         peaks.popleft()
 
+
 if peaks:
     print(f"Alex failed! He has to organize his journey better next time -> @PIRINWINS")
+else:
+    print(f"Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK")
 if conquered_peaks:
     print("Conquered peaks:")
     [print(peak) for peak in conquered_peaks]

861d3293a1a9098ff44f300bfdd86a711472e51b
Viviancat666 <vivian9069@gmail.com>
2024-02-12 00:54:23 +0200

    Create climb_the_peaks.py

---
+++ exam_preparation/climb_the_peaks.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+portions = deque([int(x) for x in input().split(", ")])
+stamina = deque([int(x) for x in input().split(", ")])
+
+peaks = deque([
+    [80, "Vihren"],
+    [90, "Kutelo"],
+    [100, "Banski Suhodol"],
+    [60, "Polezhan"],
+    [70, "Kamenitza"]
+])
+
+conquered_peaks = []
+
+for index in range(7):
+    current_portion = portions.pop()
+    current_stamina = stamina.popleft()
+    result = current_portion + current_stamina
+    if not peaks:
+        print(f"Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK")
+        break
+    current_peak_level, current_peak_name = peaks[0]
+    if result >= current_peak_level:
+        conquered_peaks.append(current_peak_name)
+        peaks.popleft()
+
+if peaks:
+    print(f"Alex failed! He has to organize his journey better next time -> @PIRINWINS")
+if conquered_peaks:
+    print("Conquered peaks:")
+    [print(peak) for peak in conquered_peaks]

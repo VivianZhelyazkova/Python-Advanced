30ab73000ed3a9c637d6257c17cf8afc68e414dc
Viviancat666 <vivian9069@gmail.com>
2024-02-12 20:45:57 +0200

    Update song_creator.py

--- exam_preparation/song_creator.py
+++ exam_preparation/song_creator.py
@@ -5,23 +5,11 @@ def add_songs(*args):
         if name not in songs:
             songs[name] = text
         else:
-            
-            songs[name].append(text)
+            songs[name].extend(text)
+
     for song, lyrics in songs.items():
-        result += f"-{song}\n"
-        if len(lyrics) > 0:
-            for line in lyrics:
-                result += f"{line}\n"
+        result += f"- {song}\n"
+        for line in lyrics:
+            result += f"{line}\n"
     return result
 
-
-print(add_songs(
-    ("Beat It", []),
-    ("Beat It",
-     ["Just beat it (beat it), beat it (beat it)",
-      "No one wants to be defeated"]),
-    ("Beat It", []),
-    ("Beat It",
-     ["Showin' how funky and strong is your fight",
-      "It doesn't matter who's wrong or right"]),
-))

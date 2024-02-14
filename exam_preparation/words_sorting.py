d8a70e2ec95dd11babb1d807d7cc85ba33784b7b
Viviancat666 <vivian9069@gmail.com>
2024-02-14 22:15:10 +0200

    Update words_sorting.py

--- exam_preparation/words_sorting.py
+++ exam_preparation/words_sorting.py
@@ -3,11 +3,8 @@ def words_sorting(*args):
     result = ""
     for word in args:
         words[word] = sum(ord(letter) for letter in word)
-
-    if sum(words.values()) % 2 != 0:
-        sorted_words = dict(sorted(words.items(), key=lambda x: x[1], reverse=True))
-    else:
-        sorted_words = dict(sorted(words.items(), key=lambda x: x[0]))
+    sorted_words = dict(sorted(words.items(), key=lambda x: -x[1] if sum(words.values()) % 2 != 0 else x[0]))
     for word, number in sorted_words.items():
         result += f"{word} - {number}\n"
     return result
+

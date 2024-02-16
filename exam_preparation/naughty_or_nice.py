506b8dc6993cb5508e3291e8cea2407ee403f08e
Viviancat666 <vivian9069@gmail.com>
2024-02-16 20:04:59 +0200

    Create naughty_or_nice.py

---
+++ exam_preparation/naughty_or_nice.py
@@ -0,0 +1,23 @@
+def naughty_or_nice_list(*args, **kwargs):
+
+    pass
+
+
+print(naughty_or_nice_list(
+    [
+        (6, "John"),
+        (4, "Karen"),
+        (2, "Tim"),
+        (1, "Merry"),
+        (6, "Frank"),
+    ],
+    "6-Nice",
+    "5-Naughty",
+    "4-Nice",
+    "3-Naughty",
+    "2-Nice",
+    "1-Naughty",
+    Frank="Nice",
+    Merry="Nice",
+    John="Naughty",
+))

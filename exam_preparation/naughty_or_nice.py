c40eb78b2f860510782d907e7bfe3b7558fdee6b
Viviancat666 <vivian9069@gmail.com>
2024-02-16 20:46:51 +0200

    Update naughty_or_nice.py

--- exam_preparation/naughty_or_nice.py
+++ exam_preparation/naughty_or_nice.py
@@ -1,23 +1,37 @@
-def naughty_or_nice_list(*args, **kwargs):
+def naughty_or_nice_list(some_list, *args, **kwargs):
+    nice_list = []
+    naughty_list = []
+    result = ""
+    for command in args:
+        number = int(command.split("-")[0])
+        cmd = command.split("-")[1]
+        matching_kids = [kid for kid in some_list if kid[0] == number]
+        if len(matching_kids) == 1:
+            kid = matching_kids[0]
+            some_list.remove(kid)
+            if cmd == "Naughty":
+                naughty_list.append(kid[1])
+            elif cmd == "Nice":
+                nice_list.append(kid[1])
 
-    pass
+    if kwargs:
+        for name, cmd in kwargs.items():
+            matching_kids = [kid for kid in some_list if kid[1] == name]
+            if len(matching_kids) == 1:
+                kid = matching_kids[0]
+                some_list.remove(kid)
+                if cmd == "Naughty":
+                    naughty_list.append(kid[1])
+                elif cmd == "Nice":
+                    nice_list.append(kid[1])
+
+    not_found_list = [kid[1] for kid in some_list]
+    if nice_list:
+        result += f"Nice: {', '.join(nice_list)}\n"
+    if naughty_list:
+        result += f"Naughty: {', '.join(naughty_list)}\n"
+    if not_found_list:
+        result += f"Not found: {', '.join(not_found_list)}"
+    return result
 
 
-print(naughty_or_nice_list(
-    [
-        (6, "John"),
-        (4, "Karen"),
-        (2, "Tim"),
-        (1, "Merry"),
-        (6, "Frank"),
-    ],
-    "6-Nice",
-    "5-Naughty",
-    "4-Nice",
-    "3-Naughty",
-    "2-Nice",
-    "1-Naughty",
-    Frank="Nice",
-    Merry="Nice",
-    John="Naughty",
-))
